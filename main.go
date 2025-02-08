fbfdcb1ec21874e689d8418fea3659d3d554e9a9
aa <oneone@gmail.com>
2025-02-08 10:52:07 -0800

    scanner

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	scanner := bufio.NewScanner(os.Stdin)
+	
+	for {
+		fmt.Print("> ")
+		scanner.Scan()
+		input := strings.TrimSpace(scanner.Text())
+		
+		if input == "quit" {
+			break
+		}
+		
+		fmt.Println("You entered:", input)
+	}
 }
