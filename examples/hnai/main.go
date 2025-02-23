b9df9743e97b65979f5da88fbd8ccfbdc48993ec
aa <oneone@gmail.com>
2025-02-22 17:31:37 -0800

    help

--- examples/hnai/main.go
+++ examples/hnai/main.go
@@ -2,30 +2,9 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"strconv"
 )
 
 func main() {
-	if len(os.Args) > 1 {
-		id, err := strconv.Atoi(os.Args[1])
-		if err != nil {
-			fmt.Println("Please provide a valid story ID")
-			return
-		}
-		story, err := fetchStoryByID(id)
-		if err != nil {
-			fmt.Println("Error fetching story:", err)
-			return
-		}
-		if story.URL != "" {
-			fmt.Printf("URL for story %d: %s\n", id, story.URL)
-		} else {
-			fmt.Printf("Story %d has no URL\n", id)
-		}
-		return
-	}
-
 	s, _ := fetchStoriesSync()
 	for _, item := range s {
 		fmt.Println(item.ID)
--- examples/hnai/stories.go
+++ examples/hnai/stories.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 )
 
@@ -25,22 +24,6 @@ func fetchStoriesSync() ([]Story, error) {
 	var storyIDs []int
 	if err := json.NewDecoder(resp.Body).Decode(&storyIDs); err != nil {
 		return nil, err
-	}
-
-	stories := make([]Story, 30)
-	for i := 0; i < 30; i++ {
-		url := fmt.Sprintf("https://hacker-news.firebaseio.com/v0/item/%d.json", storyIDs[i])
-		resp, err := http.Get(url)
-		if err != nil {
-			return nil, err
-		}
-		defer resp.Body.Close()
-
-		var story Story
-		if err := json.NewDecoder(resp.Body).Decode(&story); err != nil {
-			return nil, err
-		}
-		stories[i] = story
 	}
 
 	return stories[0:20], nil
